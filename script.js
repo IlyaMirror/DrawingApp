6d64aafa8b23ec68c5bb2bb6ccc93fc0da2e66a4
IlyaMirror <86870342+IlyaMirror@users.noreply.github.com>
2024-06-07 09:46:22 +0300

    5
    5

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const toolsBtn = document.querySelectorAll(".tool"),
 canvas = document.querySelector("canvas"),
 colorFill = document.querySelector("#fill-color"),
 sizeSlider = document.querySelector("#size-slider"),
-colorBtns = document.querySelectorAll(".colors .option");
+colorBtns = document.querySelectorAll(".colors .option"),
 colorPicker = document.querySelector("#color-picker"),
 cleanCanvas = document.querySelector(".clear-board"),
 saveCanvas = document.querySelector(".save-board"),
 redoCanvas = document.querySelector(".redo-board"),
-undoCanvas = document.querySelector(".undo-board");
+undoCanvas = document.querySelector(".undo-board"),
+pipetteBtn = document.getElementById('pipette');
 
 const ctx = canvas.getContext("2d");
 
@@ -18,37 +19,42 @@ isDrawing = false,
 prevMouseX,
 prevMouseY,
 snapShot,
+maxPressure = 0,
 undoStack = [],
 redoStack = [];
 
 const setCanvasBackground = () => {
     ctx.fillStyle = '#fff';
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = selectedColor;
 }
 
-window.addEventListener("load",() => {
+window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     canvas.style.cursor = "crosshair";
     setCanvasBackground();
-})
+});
 
 const drawRect = (e) => {
-    if(!colorFill.checked) {
-        ctx.strokeRect(e.offsetX,e.offsetY,prevMouseX - e.offsetX,prevMouseY - e.offsetY);
+    const offsetX = e.offsetX || e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    const offsetY = e.offsetY || e.touches[0].clientY - canvas.getBoundingClientRect().top;
+    if (!colorFill.checked) {
+        ctx.strokeRect(offsetX, offsetY, prevMouseX - offsetX, prevMouseY - offsetY);
     } else {
-        ctx.fillRect(e.offsetX,e.offsetY,prevMouseX - e.offsetX,prevMouseY - e.offsetY);
+        ctx.fillRect(offsetX, offsetY, prevMouseX - offsetX, prevMouseY - offsetY);
     }
 }
 
 const drawCircle = (e) => {
     ctx.beginPath();
-    const radius = Math.sqrt(Math.pow(prevMouseX - e.offsetX,2) + Math.pow(prevMouseY - e.offsetY,2));
+    const offsetX = e.offsetX || e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    const offsetY = e.offsetY || e.touches[0].clientY - canvas.getBoundingClientRect().top;
+    const radius = Math.sqrt(Math.pow(prevMouseX - offsetX, 2) + Math.pow(prevMouseY - offsetY, 2));
 
-    ctx.arc(prevMouseX,prevMouseY,radius,0,2*Math.PI);
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
 
-    if(colorFill.checked) {
+    if (colorFill.checked) {
         ctx.fill();
     } else {
         ctx.stroke();
@@ -57,11 +63,13 @@ const drawCircle = (e) => {
 
 const drawTriangle = (e) => {
     ctx.beginPath();
-    ctx.moveTo(prevMouseX,prevMouseY);
-    ctx.lineTo(e.offsetX,e.offsetY);
-    ctx.lineTo(prevMouseX * 2 - e.offsetX,e.offsetY);
+    const offsetX = e.offsetX || e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    const offsetY = e.offsetY || e.touches[0].clientY - canvas.getBoundingClientRect().top;
+    ctx.moveTo(prevMouseX, prevMouseY);
+    ctx.lineTo(offsetX, offsetY);
+    ctx.lineTo(prevMouseX * 2 - offsetX, offsetY);
     ctx.closePath();
-    if(colorFill.checked){
+    if (colorFill.checked) {
         ctx.fill();
     } else {
         ctx.stroke();
@@ -69,7 +77,7 @@ const drawTriangle = (e) => {
 }
 
 toolsBtn.forEach((btn) => {
-    btn.addEventListener("click",() =>{
+    btn.addEventListener("click", () => {
         document.querySelector('.options .active').classList.remove("active");
         btn.classList.add("active");
         selectedTool = btn.id;
@@ -77,102 +85,118 @@ toolsBtn.forEach((btn) => {
 });
 
 colorBtns.forEach((btn) => {
-    btn.addEventListener("click",() => {
+    btn.addEventListener("click", () => {
         document.querySelector('.options .selected').classList.remove("selected");
         btn.classList.add("selected");
         selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
     });
 });
 
-colorPicker.addEventListener("change",() => {
-    colorPicker.parentElement.style.background=colorPicker.value;
+colorPicker.addEventListener("change", () => {
+    colorPicker.parentElement.style.background = colorPicker.value;
     colorPicker.parentElement.click();
 });
 
 const startDraw = (e) => {
+    e.preventDefault(); // предотвращение прокрутки
     isDrawing = true;
-    prevMouseX = e.offsetX;
-    prevMouseY = e.offsetY;
+    const offsetX = e.offsetX || e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    const offsetY = e.offsetY || e.touches[0].clientY - canvas.getBoundingClientRect().top;
+    prevMouseX = offsetX;
+    prevMouseY = offsetY;
+    maxPressure = e.pressure || 0.5; // Устанавливаем начальное максимальное давление
     ctx.beginPath();
-    ctx.lineWidth = brushWidth;
+    ctx.lineWidth = brushWidth * maxPressure;
     ctx.strokeStyle = selectedColor;
     ctx.fillStyle = selectedColor;
-    snapShot = ctx.getImageData(0,0,canvas.width,canvas.height);
+    snapShot = ctx.getImageData(0, 0, canvas.width, canvas.height);
     redoStack = [];
-}; 
+};
 
 let zoomLevel = 1;
 const container = canvas.parentElement;
 
 const drawing = (e) => {
-    if(!isDrawing) return;
-    ctx.putImageData(snapShot,0,0);
-
-    if(selectedTool === "brush" || selectedTool === "eraser") {
+    if (!isDrawing) return;
+    e.preventDefault(); // предотвращение прокрутки
+    ctx.putImageData(snapShot, 0, 0);
+    const offsetX = e.offsetX || e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    const offsetY = e.offsetY || e.touches[0].clientY - canvas.getBoundingClientRect().top;
+    
+    const currentPressure = e.pressure || 0.5; // Текущее давление (по умолчанию 0.5)
+    maxPressure = Math.max(maxPressure, currentPressure); // Обновляем максимальное давление
+    
+    if (selectedTool === "brush" || selectedTool === "eraser") {
         canvas.style.cursor = "crosshair";
-
         ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
-        ctx.lineTo(e.offsetX,e.offsetY);
+        ctx.lineWidth = brushWidth * currentPressure; // Учитываем текущее давление при рисовании
+        ctx.lineTo(offsetX, offsetY);
         ctx.stroke();
-    } else if(selectedTool === "rectangle") {
+        prevMouseX = offsetX; // Обновляем предыдущие координаты
+        prevMouseY = offsetY; // Обновляем предыдущие координаты
+    } else if (selectedTool === "rectangle") {
         drawRect(e);
-    } else if(selectedTool === "circle") {
+    } else if (selectedTool === "circle") {
         drawCircle(e);
-    } else if(selectedTool === "rectangle") {
-        drawRect(e);
-    } else if(selectedTool === "zoomin") {
+    } else if (selectedTool === "zoomin") {
         canvas.style.cursor = "zoom-in";
-        zoomLevel+=0.02;
+        zoomLevel += 0.02;
         canvas.style.transform = `scale(${zoomLevel})`;
-    } else if(selectedTool === "zoomout") {
+    } else if (selectedTool === "zoomout") {
         canvas.style.cursor = "zoom-out";
-        zoomLevel-=0.02;
+        zoomLevel -= 0.02;
         canvas.style.transform = `scale(${zoomLevel})`;
-        if(zoomLevel<=1){
+        if (zoomLevel <= 1) {
             canvas.style.transform = `scale(1)`;
-            container.style.overflow="hidden";
+            container.style.overflow = "hidden";
         }
     } else {
         drawTriangle(e);
     }
 };
 
+const stopDrawing = () => {
+    isDrawing = false;
+    if (selectedTool === "brush" || selectedTool === "eraser") {
+        ctx.lineWidth = brushWidth * maxPressure; // Применяем максимальное давление
+        ctx.lineTo(prevMouseX, prevMouseY);
+        ctx.stroke();
+        ctx.beginPath(); // Начинаем новый путь, чтобы сбросить текущий контур
+    }
+    saveSnapshot();
+};
+
 const saveSnapshot = () => {
     redoStack = [];
-    undoStack.push(ctx.getImageData(0,0,canvas.width,canvas.height));
-}
+    undoStack.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+};
 
-undoCanvas.onclick=()=> {
-    if(undoStack.length > 1){
+undoCanvas.onclick = () => {
+    if (undoStack.length > 1) {
         redoStack.push(undoStack.pop());
         const prevImageData = undoStack[undoStack.length - 1];
-        ctx.putImageData(prevImageData,0,0);
-    }
-}
-
-redoCanvas.onclick=()=> {
-    if(redoStack.length > 0){
-        undoStack.push(redoStack.pop());
-        const nextImageData = undoStack[undoStack.length - 1];
-        ctx.putImageData(nextImageData,0,0);
+        ctx.putImageData(prevImageData, 0, 0);
     }
 };
 
-canvas.addEventListener("mouseup", () => {
-    isDrawing = false;
-    saveSnapshot();
-});
+redoCanvas.onclick = () => {
+    if (redoStack.length > 0) {
+        undoStack.push(redoStack.pop());
+        const nextImageData = undoStack[undoStack.length - 1];
+        ctx.putImageData(nextImageData, 0, 0);
+    }
+};
 
-sizeSlider.addEventListener("change", () => {brushWidth = sizeSlider.value});
+sizeSlider.addEventListener("change", () => { brushWidth = sizeSlider.value });
 
 cleanCanvas.onclick = () => {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     setCanvasBackground();
 };
 
 saveCanvas.onclick = () => {
     const link = document.createElement("a");
-    link.download=`${Date.now()}.jpg`;
+    link.download = `${Date.now()}.jpg`;
     link.href = canvas.toDataURL();
     link.click();
 };
@@ -205,9 +229,12 @@ window.addEventListener("keyup", (event) => {
     }
 });
 
-canvas.addEventListener("mousedown", startDraw);
-canvas.addEventListener("mousemove", drawing);
-canvas.addEventListener("mouseup", () => isDrawing = false);
+// Обработчики для планшета и мыши
+canvas.addEventListener("pointerdown", startDraw);
+canvas.addEventListener("pointermove", drawing);
+canvas.addEventListener("pointerup", stopDrawing);
+canvas.addEventListener("pointerleave", stopDrawing);
+canvas.addEventListener("pointercancel", stopDrawing);
 
 window.addEventListener("resize", () => {
     saveSnapshot();
@@ -243,4 +270,52 @@ window.addEventListener("load", () => {
         };
         img.src = canvasData;
     }
+});
+
+const getColorAtPixel = (x, y) => {
+    const pixel = ctx.getImageData(x, y, 1, 1);
+    const data = pixel.data;
+    return `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+};
+
+pipetteBtn.addEventListener('click', () => {
+    selectedTool = "pipette";
+    canvas.style.cursor = "crosshair";
+    pipette();
+});
+
+const pipette = () => {
+    const pipetteClickHandler = (e) => {
+        const x = e.offsetX || e.layerX;
+        const y = e.offsetY || e.layerY;
+        const color = getColorAtPixel(x, y);
+        selectedColor = color;
+        colorPicker.value = color;
+        colorPicker.parentElement.style.background = color;
+        
+        // Удалить обработчик события клика после его выполнения
+        canvas.removeEventListener('click', pipetteClickHandler);
+    };
+    
+    canvas.addEventListener('click', pipetteClickHandler);
+};
+
+
+pipette();
+
+const drawLine = (startX, startY, endX, endY) => {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, endY);
+    ctx.stroke();
+};
+
+
+// Предотвращение прокрутки при касании
+canvas.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+}, { passive: false });
+
+canvas.addEventListener('pointerdown', function(e) {
+    e.preventDefault();
 });
